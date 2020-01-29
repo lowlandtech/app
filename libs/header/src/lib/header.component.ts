268e07ff10d35cbb8501bb9292bb5b650e7079f1
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 10:11:53 +0100

    feat(header): add aside menu toggler directive
    $ ng generate @schematics/angular:directive --name=Aside.Menu.Toggler --project=header --export --no-flat --prefix=scx --selector=scxAsideMenuToggler --skipImport

--- libs/header/src/lib/header.component.ts
+++ libs/header/src/lib/header.component.ts
@@ -4,14 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'scx-header',
   template: `
     <header class="app-header navbar">
-      <button class="navbar-toggler d-lg-none" type="button" appMobileSidebarToggler>
+      <button class="navbar-toggler d-lg-none" type="button" scxMobileSidebarToggler>
         <span class="navbar-toggler-icon"></span>
       </button>
       <a class="navbar-brand" href="#"></a>
-      <button class="navbar-toggler d-md-down-none mr-auto" type="button" appSidebarToggler>
+      <button class="navbar-toggler d-md-down-none mr-auto" type="button" scxSidebarToggler>
         <span class="navbar-toggler-icon"></span>
       </button>
-      <button class="navbar-toggler d-md-down-none" type="button" appAsideMenuToggler>
+      <button class="navbar-toggler d-md-down-none" type="button" scxAsideMenuToggler>
         <span class="navbar-toggler-icon"></span>
       </button>
     </header>
--- libs/header/src/lib/header.module.ts
+++ libs/header/src/lib/header.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { SidebarTogglerDirective } from './sidebar.toggler';
 import { MobileSidebarTogglerDirective } from './mobile.sidebar.toggler';
+import { AsideMenuTogglerDirective } from './aside.menu.toggler';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     HeaderComponent,
     SidebarTogglerDirective,
-    MobileSidebarTogglerDirective
+    MobileSidebarTogglerDirective,
+    AsideMenuTogglerDirective
   ],
   exports: [
     HeaderComponent,
     SidebarTogglerDirective,
-    MobileSidebarTogglerDirective
+    MobileSidebarTogglerDirective,
+    AsideMenuTogglerDirective
   ]
 })
 export class HeaderModule {}
