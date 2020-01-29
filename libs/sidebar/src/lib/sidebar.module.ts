91a4082904bc52eb6db1fb16a36ec4ca6a9cb072
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 11:25:03 +0100

    feat(sidebar): add sidebar footer component
    $ ng generate @schematics/angular:component --name=sidebar-footer --project=sidebar --style=scss --export --prefix=scx --selector=scx-sidebar-footer --styleext=scss --no-interactive

--- libs/sidebar/src/lib/sidebar.module.ts
+++ libs/sidebar/src/lib/sidebar.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
+import { SidebarFooterComponent } from './sidebar-footer/sidebar-footer.component';
 
 export const sidebarRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [SidebarComponent],
-  exports: [SidebarComponent]
+  declarations: [SidebarComponent, SidebarFooterComponent],
+  exports: [SidebarComponent, SidebarFooterComponent]
 })
 export class SidebarModule {}
