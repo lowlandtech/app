e273c758037a927f1005dd4af0db77805c49cabc
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 11:36:18 +0100

    feat(sidebar): add sidebar header component
    $ ng generate @schematics/angular:component --name=sidebar-header --project=sidebar --style=scss --export --prefix=scx --selector=scx-sidebar-header --styleext=scss

--- libs/sidebar/src/lib/sidebar.module.ts
+++ libs/sidebar/src/lib/sidebar.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
 import { SidebarFooterComponent } from './sidebar-footer/sidebar-footer.component';
+import { SidebarHeaderComponent } from './sidebar-header/sidebar-header.component';
 
 export const sidebarRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [SidebarComponent, SidebarFooterComponent],
-  exports: [SidebarComponent, SidebarFooterComponent]
+  declarations: [SidebarComponent, SidebarFooterComponent, SidebarHeaderComponent],
+  exports: [SidebarComponent, SidebarFooterComponent, SidebarHeaderComponent]
 })
 export class SidebarModule {}
