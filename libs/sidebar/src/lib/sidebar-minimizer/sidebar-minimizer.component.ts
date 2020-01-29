3af0e2e1c30b3f3186b14425290d627cc99ad842
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 12:05:02 +0100

    feat(sidebar): update template

--- libs/sidebar/src/lib/sidebar-minimizer/sidebar-minimizer.component.ts
+++ libs/sidebar/src/lib/sidebar-minimizer/sidebar-minimizer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'scx-sidebar-minimizer',
-  template: '<ng-content></ng-content>'
+  template: '<button class="sidebar-minimizer" type="button" scxSidebarMinimizer scxBrandMinimizer></button>'
 })
 export class SidebarMinimizerComponent implements OnInit {
 
