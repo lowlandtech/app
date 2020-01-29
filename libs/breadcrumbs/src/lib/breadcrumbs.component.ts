874e9c68acabd031b6681ce03746a631f459f799
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 09:15:43 +0100

    feat(breadcrumbs): add new component to app module

--- libs/breadcrumbs/src/lib/breadcrumbs.component.ts
+++ libs/breadcrumbs/src/lib/breadcrumbs.component.ts
@@ -1,28 +1,33 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd, Data } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'scx-breadcrumbs',
   template: `
-  <ng-template ngFor let-breadcrumb [ngForOf]="breadcrumbs" let-last = last>
-    <li class="breadcrumb-item"
-        *ngIf="breadcrumb.label.title&&breadcrumb.url.substring(breadcrumb.url.length-1) == '/'||breadcrumb.label.title&&last"
-        [ngClass]="{active: last}">
-      <a *ngIf="!last" [routerLink]="breadcrumb.url">{{breadcrumb.label.title}}</a>
-      <span *ngIf="last" [routerLink]="breadcrumb.url">{{breadcrumb.label.title}}</span>
-    </li>
-  </ng-template>`
+  <ol class="breadcrumb">
+    <ng-template ngFor let-breadcrumb [ngForOf]="breadcrumbs" let-last = last>
+      <li class="breadcrumb-item"
+          *ngIf="breadcrumb.label.title&&breadcrumb.url.substring(breadcrumb.url.length-1) == '/'||breadcrumb.label.title&&last"
+          [ngClass]="{active: last}">
+        <a *ngIf="!last" [routerLink]="breadcrumb.url">{{breadcrumb.label.title}}</a>
+        <span *ngIf="last" [routerLink]="breadcrumb.url">{{breadcrumb.label.title}}</span>
+      </li>
+    </ng-template>
+  </ol>`
 })
 export class BreadcrumbsComponent {
-  breadcrumbs: Array<Object>;
+  breadcrumbs: Array<{
+    url: string;
+    label: Data
+  }>;
   constructor(
     private router: Router,
     private route: ActivatedRoute
   ) {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
-    ).subscribe((event) => {
+    ).subscribe(() => {
       this.breadcrumbs = [];
       let currentRoute = this.route.root,
       url = '';
