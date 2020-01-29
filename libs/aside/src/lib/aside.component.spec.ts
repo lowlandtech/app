91984586c9e88a4a62692aa50fb03a9c26ba4cc6
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 05:14:50 +0100

    feat(aside): add component and tests
    // example: https://github.com/juristr/angular-testing-recipes/blob/master/src/app/components/content-projection.component.spec.ts

--- libs/aside/src/lib/aside.component.spec.ts
+++ libs/aside/src/lib/aside.component.spec.ts
@@ -1,14 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { AsideComponent } from './aside.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'scx-aside-test',
+  template: `
+    <scx-aside>
+      <span title>I'm a test</span>
+    </scx-aside>
+  `
+})
+class AsideComponentTest {}
 
 describe('AsideComponent', () => {
   let component: AsideComponent;
+  let componentTest: AsideComponentTest;
   let fixture: ComponentFixture<AsideComponent>;
+  let fixtureTest: ComponentFixture<AsideComponentTest>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AsideComponent ]
+      declarations: [ AsideComponent, AsideComponentTest ]
     })
     .compileComponents();
   }));
@@ -17,9 +30,25 @@ describe('AsideComponent', () => {
     fixture = TestBed.createComponent(AsideComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    fixtureTest = TestBed.createComponent(AsideComponentTest);
+    componentTest = fixtureTest.componentInstance;
+    fixtureTest.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create component test', () => {
+    expect(componentTest).toBeTruthy();
+  });
+
+  it('should have projected content', () => {
+    expect(
+      fixtureTest.debugElement.query(By.css('aside')).query(By.css('span'))
+        .nativeElement.innerHTML
+    ).toContain("I'm a test");
+  });
+
 });
--- libs/aside/src/lib/aside.component.ts
+++ libs/aside/src/lib/aside.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'spotacard-aside',
-  templateUrl: './aside.component.html',
+  selector: 'scx-aside',
+  template: `
+<aside class="aside-menu">
+  <ng-content></ng-content>
+</aside>
+  `,
   styleUrls: ['./aside.component.scss']
 })
 export class AsideComponent implements OnInit {
