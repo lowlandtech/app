e8e9ed5f3bdcfe988d47d4e627d7bf33f4ca57d9
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 08:58:44 +0100

    feat(breadcrumbs): add markup

--- libs/breadcrumbs/src/lib/breadcrumbs.component.spec.ts
+++ libs/breadcrumbs/src/lib/breadcrumbs.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
 
 describe('BreadcrumbsComponent', () => {
@@ -8,7 +8,8 @@ describe('BreadcrumbsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BreadcrumbsComponent ]
+      declarations: [ BreadcrumbsComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   }));
