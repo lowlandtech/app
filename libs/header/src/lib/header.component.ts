73a55d500e2c0433efe1f5b0e90bc3e3e7fb394b
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 10:28:40 +0100

    feat(header): add hostbinding to header

--- libs/header/src/lib/header.component.ts
+++ libs/header/src/lib/header.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'scx-header',
   template: `
-    <header class="app-header navbar">
-      <button class="navbar-toggler d-lg-none" type="button" scxMobileSidebarToggler>
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="#"></a>
-      <button class="navbar-toggler d-md-down-none mr-auto" type="button" scxSidebarToggler>
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <button class="navbar-toggler d-md-down-none" type="button" scxAsideMenuToggler>
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </header>
+    <button class="navbar-toggler d-lg-none" type="button" scxMobileSidebarToggler>
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler d-md-down-none mr-auto" type="button" scxSidebarToggler>
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <button class="navbar-toggler d-md-down-none" type="button" scxAsideMenuToggler>
+      <span class="navbar-toggler-icon"></span>
+    </button>
   `,
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @HostBinding('class.app-header') class1 = true;
+  @HostBinding('class.navbar') class2 = true;
 
   constructor() { }
 
