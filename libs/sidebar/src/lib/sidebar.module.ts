c54cf4bf785188b14eb6b45082a84a6571cd5612
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-29 11:51:02 +0100

    feat(sidebar): add index files to sub folders

--- libs/sidebar/src/lib/sidebar.module.ts
+++ libs/sidebar/src/lib/sidebar.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
-import { SidebarFooterComponent } from './sidebar-footer/sidebar-footer.component';
-import { SidebarHeaderComponent } from './sidebar-header/sidebar-header.component';
-import { SidebarFormComponent } from './sidebar-form/sidebar-form.component';
-import { SidebarMinimizerComponent } from './sidebar-minimizer/sidebar-minimizer.component';
-import { SidebarNavComponent } from './sidebar-nav/sidebar-nav.component';
+import { SidebarFooterComponent } from './sidebar-footer';
+import { SidebarHeaderComponent } from './sidebar-header/';
+import { SidebarFormComponent } from './sidebar-form';
+import { SidebarMinimizerComponent } from './sidebar-minimizer';
+import { SidebarNavComponent } from './sidebar-nav';
 
 export const sidebarRoutes: Route[] = [];
 
