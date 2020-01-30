d0b3f416c43554c9e11aa42b5a71f11932bcb759
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-30 11:37:20 +0100

    feat(sidebar): test directive using ng-mocks
    // example: https://dev.to/stack-labs/no-more-angular-unit-test-1-component-template-2b09
    // example: https://stackoverflow.com/questions/36432407/how-to-unit-test-a-directive-in-angular-2

--- libs/sidebar/src/lib/nav-dropdown/nav-dropdown.directive.spec.ts
+++ libs/sidebar/src/lib/nav-dropdown/nav-dropdown.directive.spec.ts
@@ -1,8 +1,49 @@
 import { NavDropdownDirective } from './nav-dropdown.directive';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { SidebarModule } from '../sidebar.module';
+import { Component, HostBinding, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MockRender } from 'ng-mocks';
+
+@Component({
+  selector: 'scx-test-component',
+  template: ''
+})
+class TestComponent { }
 
 describe('NavDropdownDirective', () => {
+  let component: TestComponent;
+  let directiveEl: DebugElement;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        NavDropdownDirective
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = MockRender(`<scx-test-component scxNavDropdown></scx-test-component>`);
+    component = fixture.componentInstance;
+    directiveEl = fixture.debugElement.query(By.directive(NavDropdownDirective));
+    fixture.detectChanges();
+  });
+
+  it('should create a host instance', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create a directive instance', () => {
+    expect(directiveEl).toBeTruthy();
+  });
+
   it('should create an instance', () => {
-    const directive = new NavDropdownDirective();
+    const directive = directiveEl.injector.get(NavDropdownDirective);
     expect(directive).toBeTruthy();
   });
+
 });
