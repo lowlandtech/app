14ef4b44d8080254d8a9bf103b89f418b9091d8b
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-30 15:56:18 +0100

    feat(sidebar): add code to directive

--- libs/sidebar/src/lib/nav-dropdown/nav-dropdown-toggle.directive.ts
+++ libs/sidebar/src/lib/nav-dropdown/nav-dropdown-toggle.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
+import { NavDropdownDirective } from './nav-dropdown.directive';
 
 @Directive({
   selector: '[scxNavDropdownToggle]'
 })
 export class NavDropdownToggleDirective {
+  constructor(private dropdown: NavDropdownDirective) {}
 
-  constructor() { }
-
+  @HostListener('click', ['$event'])
+  toggleOpen($event: any) {
+    $event.preventDefault();
+    this.dropdown.toggle();
+  }
 }
