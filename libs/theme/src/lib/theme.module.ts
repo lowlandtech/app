8056cdd276457edbfd064e567e3af318890fd6d9
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-31 06:37:18 +0100

    feat(theme): add variable service
    $ ng generate @schematics/angular:service --name=Variables --project=theme --no-interactive

--- libs/theme/src/lib/theme.module.ts
+++ libs/theme/src/lib/theme.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import jss from 'jss';
 import preset from 'jss-preset-default';
+import { VariablesService } from './variables/variables.service';
 
 jss.setup(preset());
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  providers: [VariablesService]
 })
-export class ThemeModule {}
+export class ThemeModule { }
