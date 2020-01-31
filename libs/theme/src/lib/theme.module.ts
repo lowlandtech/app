3e8924d0c172647c6a4f77111d19a0716e4af7cc
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-31 05:20:41 +0100

    feat(theme): configure jss

--- libs/theme/src/lib/theme.module.ts
+++ libs/theme/src/lib/theme.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import jss from 'jss';
+import preset from 'jss-preset-default';
+
+jss.setup(preset());
 
 @NgModule({
   imports: [CommonModule]
