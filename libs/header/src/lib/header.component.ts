0061a27a7a702c28193b4b05b1664828ce04ee16
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-01-31 19:45:39 +0100

    feat(header): add logo and remove style file

--- libs/header/src/lib/header.component.ts
+++ libs/header/src/lib/header.component.ts
@@ -6,15 +6,17 @@ import { Component, OnInit, HostBinding } from '@angular/core';
     <button class="navbar-toggler d-lg-none" type="button" scxMobileSidebarToggler>
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#"></a>
+    <a class="navbar-brand" href="#">
+    <span class=" raised-button small-logo text-primary">CS</span>
+    <span class="logo">Spotacard</span>
+  </a>
     <button class="navbar-toggler d-md-down-none mr-auto" type="button" scxSidebarToggler>
       <span class="navbar-toggler-icon"></span>
     </button>
     <button class="navbar-toggler d-md-down-none" type="button" scxAsideMenuToggler>
       <span class="navbar-toggler-icon"></span>
     </button>
-  `,
-  styleUrls: ['./header.component.scss']
+  `
 })
 export class HeaderComponent implements OnInit {
   @HostBinding('class.app-header') class1 = true;
