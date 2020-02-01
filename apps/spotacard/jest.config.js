cae7d685768a9b2d921e1952512a5017f5a0c343
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-01 15:19:50 +0100

    chore: remove spotacard app

--- apps/spotacard/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'spotacard',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/spotacard',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
