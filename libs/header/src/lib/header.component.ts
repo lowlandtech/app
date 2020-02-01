0e98a0ed2c465f8ed8d002a434dcbd22a0b7e1e9
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-01 15:08:33 +0100

    feat(header) #15: format file

--- libs/header/src/lib/header.component.ts
+++ libs/header/src/lib/header.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 @Component({
   selector: 'scx-header',
   template: `
-    <button class="navbar-toggler d-lg-none" type="button" scxMobileSidebarToggler>
+    <button
+      class="navbar-toggler d-lg-none"
+      type="button"
+      scxMobileSidebarToggler
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand" href="#">
-    <span class=" raised-button small-logo text-primary">CS</span>
-    <span class="logo">Spotacard</span>
-  </a>
-    <button class="navbar-toggler d-md-down-none mr-auto" type="button" scxSidebarToggler>
+      <span class=" raised-button small-logo text-primary">CS</span>
+      <span class="logo">Spotacard</span>
+    </a>
+    <button
+      class="navbar-toggler d-md-down-none mr-auto"
+      type="button"
+      scxSidebarToggler
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
     <ng-content></ng-content>
@@ -27,9 +35,7 @@ export class HeaderComponent implements OnInit {
   @HostBinding('class.app-header') class1 = true;
   @HostBinding('class.navbar') class2 = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
