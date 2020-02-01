18d1a2a785a40fe59edec7d10e82100a65e5b72c
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-01 10:49:46 +0100

    feat(social) #15: add new files

---
+++ frontend/libs/home/src/lib/+state/home.selectors.ts
@@ -0,0 +1,13 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { Home } from './home.reducer';
+
+export const getHome = createFeatureSelector<Home>('home');
+export const getTags = createSelector(
+  getHome,
+  (state: Home) => state.tags,
+);
+
+export const homeQuery = {
+  getTags,
+  getHome,
+};
