c360c60f9e714b38378121b871aabb9ee86bfbcd
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-02 16:34:50 +0100

    feat(aside): import document via angular inject (#17)
    // example: https://stackoverflow.com/questions/44189149/how-to-inject-document-body-in-angular-2

--- libs/aside/src/lib/aside.toggler/aside.toggler.directive.ts
+++ libs/aside/src/lib/aside.toggler/aside.toggler.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, HostListener, ElementRef, HostBinding } from '@angular/core';
+import { Directive, HostListener, ElementRef, HostBinding, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 /**
 * Allows the aside to be toggled via click.
@@ -9,14 +10,16 @@ import { Directive, HostListener, ElementRef, HostBinding } from '@angular/core'
 export class AsideTogglerDirective {
   @HostBinding('class.d-md-down-none') class1 = true;
 
-  constructor(private elementRef:ElementRef){
+  constructor(
+    private elementRef:ElementRef,
+    @Inject(DOCUMENT) private document: Document){
     this.elementRef.nativeElement.innerHTML ='<span class="navbar-toggler-icon"></span>';
   }
 
   @HostListener('click', ['$event'])
   toggleOpen($event: any) {
     $event.preventDefault();
-    document.querySelector('body').classList.toggle('aside-menu-hidden');
+    this.document.querySelector('body').classList.toggle('aside-menu-hidden');
   }
 }
 
