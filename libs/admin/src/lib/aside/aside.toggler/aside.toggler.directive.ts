428d1a941fea1628a88a6a22973e32102841504e
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-05 19:44:27 +0100

    feat(admin): persist aside state (#17)

--- libs/admin/src/lib/aside/aside.toggler/aside.toggler.directive.ts
+++ libs/admin/src/lib/aside/aside.toggler/aside.toggler.directive.ts
@@ -1,5 +1,10 @@
-import { Directive, HostListener, ElementRef, HostBinding, Inject } from '@angular/core';
+import { Directive, HostListener, ElementRef, HostBinding, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { AdminStateFacade } from '../../+state';
+
+const hasClass = (target: any, elementClassName: string) => {
+  return new RegExp('(\\s|^)' + elementClassName + '(\\s|$)').test(target.className);
+};
 
 /**
 * Allows the aside to be toggled via click.
@@ -7,21 +12,37 @@ import { DOCUMENT } from '@angular/common';
 @Directive({
   selector: '[scxAsideToggler]'
 })
-export class AsideTogglerDirective {
+export class AsideTogglerDirective implements OnInit {
   @HostBinding('class.d-md-down-none') class1 = true;
+  private className = 'aside-menu-hidden';
+  private element: HTMLBodyElement;
 
   constructor(
+    private facade: AdminStateFacade,
     private elementRef:ElementRef,
     @Inject(DOCUMENT) private document: Document){
     this.elementRef.nativeElement.innerHTML ='<span class="navbar-toggler-icon"></span>';
+    this.element = this.document.querySelector('body');
+  }
+
+  public ngOnInit() {
+    this.facade.aside$.subscribe(aside => {
+      if (aside.hidden) {
+        this.element.classList.add(this.className);
+      } else {
+        this.element.classList.remove(this.className);
+      }
+    });
   }
 
   @HostListener('click', ['$event'])
   toggleOpen($event: any) {
     $event.preventDefault();
-    // this.document.querySelector('body')
-    //     .classList.toggle('aside-menu-hidden');
-    // this.facade.toggle();
+    if(!hasClass(this.element, this.className)) {
+      this.facade.asideHide();
+    } else {
+      this.facade.asideShow();
+    }
   }
 }
 
