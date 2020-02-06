95e48d71d614e8c15fb0ebde16b6b3e134aa6643
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-06 19:52:57 +0100

    feat(ui): add card header component (#25)
    $ ng generate @schematics/angular:component --name=card/header --project=ui --style=scss --export --inlineStyle --inlineTemplate

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './card/header/header.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CardComponent],
-  exports: [CardComponent]
+  declarations: [CardComponent, HeaderComponent],
+  exports: [CardComponent, HeaderComponent]
 })
 export class UiModule {}
