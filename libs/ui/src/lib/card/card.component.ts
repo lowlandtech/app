fc435696d129aba3daf0bd151c91e8514658a732
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-06 19:50:37 +0100

    feat(ui): add card component (#25)
    $ ng generate @schematics/angular:component --name=card --project=ui --style=scss --export --inlineStyle --inlineTemplate

---
+++ libs/ui/src/lib/card/card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'scx-ui-card',
+  template: `
+    <p>
+      card works!
+    </p>
+  `,
+  styles: []
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
