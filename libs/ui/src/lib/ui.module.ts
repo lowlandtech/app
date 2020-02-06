b7f6381f032515f9f1dd03ddfce2af8c5767a170
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-06 20:21:58 +0100

    feat(ui): add card footer component (#25)
    $ ng generate @schematics/angular:component --name=card/footer --project=ui --style=scss --export --inlineStyle --inlineTemplate

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { HeaderComponent } from './card/header/header.component';
+import { FooterComponent } from './card/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CardComponent, HeaderComponent],
-  exports: [CardComponent, HeaderComponent]
+  declarations: [CardComponent, HeaderComponent, FooterComponent],
+  exports: [CardComponent, HeaderComponent, FooterComponent]
 })
 export class UiModule {}
