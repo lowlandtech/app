2f2e3cbc4cfdbdb62f4562abb186392d16dddeaa
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-06 18:27:06 +0100

    feat(admin): generate playground page component (#24)
    $  ng generate @schematics/angular:component --name=playground --project=playground --style=scss --export --flat --inlineTemplate --prefix=scx

---
+++ libs/playground/src/lib/playground.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'scx-playground',
+  template: `
+    <p>
+      playground works!
+    </p>
+  `,
+  styles: []
+})
+export class PlaygroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/playground/src/lib/playground.module.ts
+++ libs/playground/src/lib/playground.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { PlaygroundComponent } from './playground.component';
 
 export const playgroundRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule],
+  declarations: [PlaygroundComponent],
+  exports: [PlaygroundComponent]
 })
 export class PlaygroundModule {}
