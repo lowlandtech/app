0525d1b506111ae2e82b61504caf9742bf0de330
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-06 20:52:52 +0100

    feat(ui): add folder references (#25)

---
+++ libs/ui/src/lib/card/index.ts
@@ -0,0 +1,6 @@
+export * from './card.component';
+export * from './body';
+export * from './footer';
+export * from './header';
+export * from './toolbar';
+
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from './card/card.component';
-import { HeaderComponent } from './card/header/header.component';
-import { FooterComponent } from './card/footer/footer.component';
-import { BodyComponent } from './card/body/body.component';
-import { ToolbarComponent } from './card/toolbar/toolbar.component';
+import { CardComponent } from './card';
+import { HeaderComponent } from './card';
+import { FooterComponent } from './card';
+import { BodyComponent } from './card';
+import { ToolbarComponent } from './card';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [CardComponent, HeaderComponent, FooterComponent, BodyComponent, ToolbarComponent],
-  exports: [CardComponent, HeaderComponent, FooterComponent, BodyComponent, ToolbarComponent]
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CardComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    ToolbarComponent
+  ],
+  exports: [
+    CardComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    ToolbarComponent
+  ]
 })
 export class UiModule {}
