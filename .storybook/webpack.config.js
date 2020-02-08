512fb55f47ef1dae9a3cac365c24b5a2a7d97e34
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-08 16:26:53 +0100

    feat(ui:card): add storybook source loader (#25)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,16 @@
-// Export a function. Accept the base config as the only param.
+const path = require('path');
+
 module.exports = async ({ config, mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
-
-  // Make whatever fine-grained changes you need
-
-  // Return the altered config
+  config.module.rules.push({
+    test: [/\.stories\.tsx?$/, /index\.ts$/],
+    loaders: [
+      {
+        loader: require.resolve('@storybook/source-loader'),
+        options: {
+          parser: 'typescript'
+        }
+      }
+    ]
+  });
   return config;
 };
