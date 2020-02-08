252a6e7948ed5d56a4c336bd055a2d4dc0283bf3
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-08 16:27:56 +0100

    feat(ui:card): change story name (#25)

--- libs/ui/src/lib/card/card.component.stories.ts
+++ libs/ui/src/lib/card/card.component.stories.ts
@@ -7,12 +7,14 @@ import {
 } from '@storybook/addon-knobs';
 import { UiModule } from '../ui.module';
 import { CardComponent } from './card.component';
+import { storiesOf, moduleMetadata } from '@storybook/angular';
 
 export default {
-  title: 'Card',
+  title: '<scx-ui-card>',
   component: 'CardComponent',
   decorators: [withKnobs]
 };
+
 
 const cardData = {
   id: '1',
@@ -22,50 +24,53 @@ const cardData = {
   tags: ['tag1', 'tag2']
 };
 
-export const complete = () => ({
-  name: 'Complete',
-  moduleMetadata: {
-    imports: [UiModule]
-  },
-  component: CardComponent,
-  /* #region template */
-  template: `
-  <div class="container bg-faded py-3">
-    <div class="row">
-        <div class="col center">
-          <scx-ui-card>
-            <scx-ui-card-header>
-              <scx-ui-card-title>
-                {{card.title}}
-              </scx-ui-card-title>
-              <scx-ui-card-toolbar>
-                <div class="btn-group" role="group" aria-label="First group">
-                  <button class="btn btn-primary btn-sm" type="button">1</button>
-                  <button class="btn btn-primary btn-sm" type="button">2</button>
-                  <button class="btn btn-primary btn-sm" type="button">3</button>
-                  <button class="btn btn-primary btn-sm" type="button">4</button>
-                </div>
-              </scx-ui-card-toolbar>
-            </scx-ui-card-header>
-            <scx-ui-card-body>
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-            </scx-ui-card-body>
-            <scx-ui-card-footer>
-              <div class="fa-pull-right">
-                <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
-              </div>
-            </scx-ui-card-footer>
-          </scx-ui-card>
+storiesOf('@spotacard/ui/<scx-ui-card>', module)
+  .addDecorator(
+    moduleMetadata({
+      imports: [ UiModule ]
+    }),
+  )
+  .add('complete', () => ({
+    /* #region template */
+    template: `
+      <div class="container bg-faded py-3">
+        <div class="row">
+            <div class="col center">
+              <scx-ui-card>
+                <scx-ui-card-header>
+                  <scx-ui-card-title>
+                    {{card.title}}
+                  </scx-ui-card-title>
+                  <scx-ui-card-toolbar>
+                    <div class="btn-group" role="group" aria-label="First group">
+                      <button class="btn btn-primary btn-sm" type="button">1</button>
+                      <button class="btn btn-primary btn-sm" type="button">2</button>
+                      <button class="btn btn-primary btn-sm" type="button">3</button>
+                      <button class="btn btn-primary btn-sm" type="button">4</button>
+                    </div>
+                  </scx-ui-card-toolbar>
+                </scx-ui-card-header>
+                <scx-ui-card-body>
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">
+                    With supporting text below as a natural lead-in to additional
+                    content.
+                  </p>
+                </scx-ui-card-body>
+                <scx-ui-card-footer>
+                  <div class="fa-pull-right">
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                  </div>
+                </scx-ui-card-footer>
+              </scx-ui-card>
+            </div>
         </div>
-    </div>
-  </div>
-  `,
-  /* #endregion */
-  props: {
-    card: object('card', { ...cardData })
-  }
-});
+      </div>
+    `,
+    /* #endregion */
+    component: CardComponent,
+    props: {
+      card: object('card', { ...cardData })
+    },
+  })
+);
