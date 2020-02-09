89dc322cce760a70df6796f9342ba94cdfad0b0f
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-09 11:05:12 +0100

    feat(ui:card): replace example template (#25)

--- libs/ui/src/lib/card/card.component.stories.ts
+++ libs/ui/src/lib/card/card.component.stories.ts
@@ -5,25 +5,16 @@ import {
   withKnobs,
   object
 } from '@storybook/addon-knobs';
-import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { CardModule } from './card.module';
+import { UiModule } from '../ui.module';
 import { CardComponent } from './card.component';
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { storiesOf, moduleMetadata } from '@storybook/angular';
 
 export default {
   title: '<scx-ui-card>',
   component: 'CardComponent',
-  decorators: [
-    moduleMetadata({
-      imports: [CardModule],
-      schemas: [
-        CUSTOM_ELEMENTS_SCHEMA,
-        NO_ERRORS_SCHEMA
-      ]
-    }),
-    withKnobs
-  ]
+  decorators: [withKnobs]
 };
+
 
 const cardData = {
   id: '1',
@@ -33,40 +24,46 @@ const cardData = {
   tags: ['tag1', 'tag2']
 };
 
-storiesOf('@spotacard/ui/<scx-ui-card>', module).add('complete', () => ({
-  /* #region template */
-  template: `
-      <div class="container bg-faded py-3">
-        <div class="row">
-          <div class="col col-md-12 center">
-            <scx-ui-card>
-              <scx-ui-card-title>
-                Featured
-              </scx-ui-card-title>
-              <scx-ui-card-toolbar>
-                <button class="btn btn-primary btn-sm" type="button">1</button>
-                <button class="btn btn-primary btn-sm" type="button">2</button>
-              </scx-ui-card-toolbar>
-              <scx-ui-card-body>
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </p>
-              </scx-ui-card-body>
-              <scx-ui-card-footer>
-                <div class="fa-pull-right">
-                  <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
-                </div>
-              </scx-ui-card-footer>
-            </scx-ui-card>
-          </div>
+storiesOf('@spotacard/ui/<scx-ui-card>', module)
+  .addDecorator(
+    moduleMetadata({
+      imports: [ UiModule ]
+    }),
+  )
+  .add('complete', () => ({
+    /* #region template */
+    template: `
+    <div class="container bg-faded py-3">
+      <div class="row">
+        <div class="col col-md-12 center">
+          <scx-ui-card>
+            <div class="card-header-title">
+              Featured
+            </div>
+            <div class="card-toolbar">
+              <button class="btn btn-primary btn-sm" type="button">1</button>
+              <button class="btn btn-primary btn-sm" type="button">2</button>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional content.
+              </p>
+            </div>
+            <div class="card-footer">
+              <div class="fa-pull-right">
+                <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+              </div>
+            </div>
+          </scx-ui-card>
         </div>
       </div>
+    </div>
     `,
-  /* #endregion */
-  component: CardComponent,
-  props: {
-    card: object('card', { ...cardData })
-  }
-}));
+    /* #endregion */
+    component: CardComponent,
+    props: {
+      card: object('card', { ...cardData })
+    },
+  })
+);
