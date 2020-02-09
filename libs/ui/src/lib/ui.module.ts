72927e002ffb040b3d9ed351b2323517ef52dc3b
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-09 12:30:27 +0100

    feat(ui:card): add default buttons to card footer (#25)

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from './card/card.module';
 
-
 @NgModule({
   imports: [
     CommonModule,
