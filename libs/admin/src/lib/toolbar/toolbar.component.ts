03653338fa9e9ddeed51a37f54ae6e757b9cd912
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-19 21:50:37 +0100

    feat(toolbar): rename bredcrumbs to toolbar (#17)

--- libs/admin/src/lib/toolbar/toolbar.component.ts
+++ libs/admin/src/lib/toolbar/toolbar.component.ts
@@ -4,49 +4,38 @@ import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'scx-toolbar',
-  template: `
-  <ol class="breadcrumb">
-    <ng-template ngFor let-toolbar [ngForOf]="toolbar" let-last = last>
-      <li class="breadcrumb-item"
-          *ngIf="toolbar.label.title&&toolbar.url.substring(toolbar.url.length-1) == '/'||toolbar.label.title&&last"
-          [ngClass]="{active: last}">
-        <a *ngIf="!last" [routerLink]="toolbar.url">{{toolbar.label.title}}</a>
-        <span *ngIf="last" [routerLink]="toolbar.url">{{toolbar.label.title}}</span>
-      </li>
-    </ng-template>
-  </ol>`
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarsComponent {
   toolbar: Array<{
     url: string;
-    label: Data
+    label: Data;
   }>;
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute
-  ) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      this.toolbar = [];
-      let currentRoute = this.route.root,
-      url = '';
-      do {
-        const childrenRoutes = currentRoute.children;
-        currentRoute = null;
-        // tslint:disable-next-line:no-shadowed-variable
-        childrenRoutes.forEach(route => {
-          if (route.outlet === 'primary') {
-            const routeSnapshot = route.snapshot;
-            url += '/' + routeSnapshot.url.map(segment => segment.path).join('/');
-            this.toolbar.push({
-              label: route.snapshot.data,
-              url:   url
-            });
-            currentRoute = route;
-          }
-        });
-      } while (currentRoute);
-    });
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.toolbar = [];
+        let currentRoute = this.route.root,
+          url = '';
+        do {
+          const childrenRoutes = currentRoute.children;
+          currentRoute = null;
+          // tslint:disable-next-line:no-shadowed-variable
+          childrenRoutes.forEach(route => {
+            if (route.outlet === 'primary') {
+              const routeSnapshot = route.snapshot;
+              url +=
+                '/' + routeSnapshot.url.map(segment => segment.path).join('/');
+              this.toolbar.push({
+                label: route.snapshot.data,
+                url: url
+              });
+              currentRoute = route;
+            }
+          });
+        } while (currentRoute);
+      });
   }
 }
