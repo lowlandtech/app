a51acfe36d847ee640005ef3a5adc13f52f8cfb3
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-23 22:06:38 +0100

    chore: reformat document

--- libs/ui/src/lib/card/card.module.ts
+++ libs/ui/src/lib/card/card.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { NxModule } from '@nrwl/angular';
 import { SharedModule } from '@spotacard/shared';
+
+import {
+  initialState,
+  metaReducers,
+  CARD_FEATURE_KEY,
+  reducer,
+  CardFacade
+} from './+state';
+
 import { CardComponent } from './card.component';
 import { CloseableComponent } from './closeable';
-import { StoreModule } from '@ngrx/store';
-import * as fromCard from './+state/card.reducer';
-import { CardFacade } from './+state/card.facade';
-import { NxModule } from '@nrwl/angular';
-import { initialState, metaReducers } from './+state/card.reducer';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     NxModule.forRoot(),
-    StoreModule.forFeature(fromCard.CARD_FEATURE_KEY, fromCard.reducer, {
+    StoreModule.forFeature(CARD_FEATURE_KEY, reducer, {
       initialState: initialState,
       metaReducers: metaReducers
-    }),
+    })
   ],
   declarations: [CardComponent, CloseableComponent],
   exports: [CardComponent, CloseableComponent],
