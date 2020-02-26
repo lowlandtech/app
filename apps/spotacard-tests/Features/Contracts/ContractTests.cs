efb2653a02acd9ec6db13349bc50a2ee29d3c513
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-26 17:42:38 +0100

    chore: reformat file

--- apps/spotacard-tests/Features/Contracts/ContractTests.cs
+++ apps/spotacard-tests/Features/Contracts/ContractTests.cs
@@ -13,97 +13,98 @@ using Spotacard.Features.Workflows;
 
 namespace Spotacard.Features.Contracts
 {
-  public class ContractTests
-  {
-    private readonly CardFixtures _fixture;
-
-    public ContractTests()
+    public class ContractTests
     {
-      _fixture = new CardFixtures();
+        private readonly CardFixtures _fixture;
+
+        public ContractTests()
+        {
+            _fixture = new CardFixtures();
+        }
+
+        [Test]
+        public void ShouldResolveTemplate()
+        {
+            var resolver = new ContractResolver();
+            var contract = resolver.Get(ContractTypes.Template, _fixture.Template);
+            Assert.That(contract, Is.InstanceOf(typeof(TemplateContract)));
+            Assert.That(contract.Card, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldResolveTodo()
+        {
+            var resolver = new ContractResolver();
+            var contract = resolver.Get(ContractTypes.Todo, _fixture.Todo);
+            Assert.That(contract, Is.InstanceOf(typeof(TodoContract)));
+            Assert.That(contract.Card, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldResolveTask()
+        {
+            var resolver = new ContractResolver();
+            var contract = resolver.Get(ContractTypes.Task, _fixture.Task);
+            Assert.That(contract, Is.InstanceOf(typeof(TaskContract)));
+            Assert.That(contract.Card, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldResolveTaskList()
+        {
+            var resolver = new ContractResolver();
+            var contract = resolver.Get(ContractTypes.TaskList, _fixture.TaskList);
+            Assert.That(contract, Is.InstanceOf(typeof(TaskListContract)));
+            Assert.That(contract.Card, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldResolveApp()
+        {
+            var resolver = new ContractResolver();
+            var contract = resolver.Get(ContractTypes.App, _fixture.App);
+            Assert.That(contract, Is.InstanceOf(typeof(AppContract)));
+            Assert.That(contract.Card, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldResolveWorkflow()
+        {
+            var resolver = new ContractResolver();
+            var contract = resolver.Get(ContractTypes.Workflow, _fixture.Workflow);
+            Assert.That(contract, Is.InstanceOf(typeof(WorkflowContract)));
+            Assert.That(contract.Card, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldResolveStep()
+        {
+            var resolver = new ContractResolver();
+            var contract = resolver.Get(ContractTypes.Step, _fixture.Step);
+            Assert.That(contract, Is.InstanceOf(typeof(StepContract)));
+            Assert.That(contract.Card, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldResolveProject()
+        {
+            var resolver = new ContractResolver();
+            var contract = resolver.Get(ContractTypes.Project, _fixture.Project);
+            Assert.That(contract, Is.InstanceOf(typeof(ProjectContract)));
+            Assert.That(contract.Card, Is.Not.Null);
+        }
     }
 
-    [Test]
-    public void ShouldResolveTemplate()
+    public class CardFixtures
     {
-      var resolver = new ContractResolver();
-      var contract = resolver.Get(ContractTypes.Template, _fixture.Template);
-      Assert.That(contract, Is.InstanceOf(typeof(TemplateContract)));
-      Assert.That(contract.Card, Is.Not.Null);
+        public Card Blog { get; set; }
+        public Card Todo { get; set; }
+        public Card Template { get; set; }
+        public Card App { get; set; }
+        public Card Workflow { get; set; }
+        public Card Step { get; set; }
+        public Card Task { get; set; }
+        public Card Project { get; set; }
+        public Card TaskList { get; set; }
     }
-
-    [Test]
-    public void ShouldResolveTodo()
-    {
-      var resolver = new ContractResolver();
-      var contract = resolver.Get(ContractTypes.Todo, _fixture.Todo);
-      Assert.That(contract, Is.InstanceOf(typeof(TodoContract)));
-      Assert.That(contract.Card, Is.Not.Null);
-    }
-
-    [Test]
-    public void ShouldResolveTask()
-    {
-      var resolver = new ContractResolver();
-      var contract = resolver.Get(ContractTypes.Task, _fixture.Task);
-      Assert.That(contract, Is.InstanceOf(typeof(TaskContract)));
-      Assert.That(contract.Card, Is.Not.Null);
-    }
-
-    [Test]
-    public void ShouldResolveTaskList()
-    {
-      var resolver = new ContractResolver();
-      var contract = resolver.Get(ContractTypes.TaskList, _fixture.TaskList);
-      Assert.That(contract, Is.InstanceOf(typeof(TaskListContract)));
-      Assert.That(contract.Card, Is.Not.Null);
-    }
-
-    [Test]
-    public void ShouldResolveApp()
-    {
-      var resolver = new ContractResolver();
-      var contract = resolver.Get(ContractTypes.App, _fixture.App);
-      Assert.That(contract, Is.InstanceOf(typeof(AppContract)));
-      Assert.That(contract.Card, Is.Not.Null);
-    }
-
-    [Test]
-    public void ShouldResolveWorkflow()
-    {
-      var resolver = new ContractResolver();
-      var contract = resolver.Get(ContractTypes.Workflow, _fixture.Workflow);
-      Assert.That(contract, Is.InstanceOf(typeof(WorkflowContract)));
-      Assert.That(contract.Card, Is.Not.Null);
-    }
-
-    [Test]
-    public void ShouldResolveStep()
-    {
-      var resolver = new ContractResolver();
-      var contract = resolver.Get(ContractTypes.Step, _fixture.Step);
-      Assert.That(contract, Is.InstanceOf(typeof(StepContract)));
-      Assert.That(contract.Card, Is.Not.Null);
-    }
-
-    [Test]
-    public void ShouldResolveProject()
-    {
-      var resolver = new ContractResolver();
-      var contract = resolver.Get(ContractTypes.Project, _fixture.Project);
-      Assert.That(contract, Is.InstanceOf(typeof(ProjectContract)));
-      Assert.That(contract.Card, Is.Not.Null);
-    }
-  }
-
-  public class CardFixtures
-  {
-    public Card Template { get; set; }
-    public Card Todo { get; set; }
-    public Card Task { get; set; }
-    public Card Workflow { get; set; }
-    public Card Step { get; set; }
-    public Card Project { get; set; }
-    public Card TaskList { get; set; }
-    public Card App { get; set; }
-  }
 }
