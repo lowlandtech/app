23c4356201262ffbe3e1c195c9b07dba6af4bebf
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-02-26 04:32:44 +0100

    feat(templates): add ef entity attribute (#28)

---
+++ apps/spotacard-api/Domain/CardAttribute.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Spotacard.Domain
+{
+  public class CardAttribute
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Value { get; set; }
+    public int Index { get; set; }
+    public Card Card { get; set; }
+    public Guid CardId { get; set; }
+  }
+}
