ef0e6a85699e6ba45d5fa8389d74cc88ae2c8571
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-04-18 06:00:07 +0200

    chore: update app

--- publish.cmd
+++ publish.cmd
@@ -1,8 +1,8 @@
-git add --all
-git commit -m "chore: update app"
-npm version patch --clean --message "chore: bump and tag version"
+npm version patch --message "chore: bump and tag version"
 MKDIR .params
 node -p "require('./package.json').version" > .params/version
 SET /p VERSION=<.params/version
+git add --all
+git commit -m "chore: update app"
 git push origin --all
   
